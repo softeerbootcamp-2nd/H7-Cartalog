0e790cae333d52e32641295e27717b8be68fbe51
ijehyunpark <adam991217@naver.com>
2023-07-31 17:45:19 +0900

    establish: Spring Boot 초기 설정

---
+++ backend/src/main/java/softeer/wantcar/cartalog/CartalogApplication.java
@@ -0,0 +1,13 @@
+package softeer.wantcar.cartalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartalogApplication.class, args);
+	}
+
+}
