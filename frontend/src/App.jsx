3612f43fc7aa1dcdfddb5161a2ae606efad72fb5
Jo <tockiib@gmail.com>
2023-08-01 18:45:55 +0900

    feat: react router dom 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+
 function App() {
-  return <>test</>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
