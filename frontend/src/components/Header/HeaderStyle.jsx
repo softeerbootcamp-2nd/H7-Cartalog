000a85d6ccf57256397ceae3f55a9b526e60d888
Jo <tockiib@gmail.com>
2023-08-03 11:15:12 +0900

    style: Header 스타일 수정

--- frontend/src/components/Header/HeaderStyle.jsx
+++ frontend/src/components/Header/HeaderStyle.jsx
@@ -5,10 +5,12 @@ export const Header = styled.header`
   justify-content: space-between;
   position: fixed;
   top: 0;
+  z-index: 1;
   width: 100%;
   height: 60px;
+  background-color: ${({ theme }) => theme.color.white};
   border-bottom: 2px solid ${({ theme }) => theme.color.gray['200']};
-  font: ${({ theme }) => theme.font.KRBold32};
+  font: ${({ theme }) => theme.font.textKR.Medium14};
 `;
 
 export const Contents = styled.div`
@@ -18,6 +20,6 @@ export const Contents = styled.div`
   justify-content: space-between;
   width: 1280px;
   margin: 0 auto;
-  padding: 0 128px;
+  padding: 0 69px;
   box-sizing: border-box;
 `;
