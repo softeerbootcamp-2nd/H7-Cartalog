076fe32b85c4ee532e0e45fff232c84fee580731
Jo <tockiib@gmail.com>
2023-08-03 15:09:40 +0900

    feat: 트림별 이미지 보기 애니메이션 추가

---
+++ frontend/src/pages/Home/Section/TrimImages/TrimImages.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import * as S from './TrimImagesStyle';
+
+const IMG_DIRECTORY = '../../../../../assets/images/TrimSelect/';
+
+function TrimImages({ trim }) {
+  const IMAGE_NAME = ['exterior', 'interior', 'wheel'];
+  const [page, setPage] = useState(0);
+
+  return (
+    <S.TrimImages>
+      {IMAGE_NAME.map((name, index) => {
+        const src = `${IMG_DIRECTORY}${trim}/${name}.png`;
+        const handleClick = () => setPage(index);
+        const className = index === page ? 'selected' : null;
+
+        return (
+          <S.TrimImageItem key={index} className={className} onClick={handleClick}>
+            <img src={src} alt={name} />
+          </S.TrimImageItem>
+        );
+      })}
+    </S.TrimImages>
+  );
+}
+
+export default TrimImages;
