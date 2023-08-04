7d2dd83d0495b4074ac32f1ff4a9805b1b80e975
Glacier Han <61905052+Glacier-Han@users.noreply.github.com>
2023-08-04 13:31:57 +0900

    feat: TypeFragment 내 커스텀뷰 옵션 선택기능 추가 및 UI대응

--- Android/app/src/main/java/com/softeer/cartalog/ui/fragment/OptionFragment.kt
+++ Android/app/src/main/java/com/softeer/cartalog/ui/fragment/OptionFragment.kt
@@ -1,6 +1,32 @@
 package com.softeer.cartalog.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.softeer.cartalog.databinding.FragmentOptionBinding
+import com.softeer.cartalog.databinding.FragmentTrimBinding
 
 class OptionFragment: Fragment() {
+    private var _binding: FragmentOptionBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOptionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
