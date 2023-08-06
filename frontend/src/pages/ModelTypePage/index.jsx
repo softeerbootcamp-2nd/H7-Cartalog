ccdf62b5ba1e05f5aa84fc8036064e069598138f
afk0323 <jwv0323@naver.com>
2023-08-06 23:56:50 +0900

    feat: 모델타입 Info 영역 구현

--- frontend/src/pages/ModelTypePage/index.jsx
+++ frontend/src/pages/ModelTypePage/index.jsx
@@ -3,10 +3,12 @@ import Info from './Info';
 import Pick from './Pick';
 
 const TYPE = 'ModelType';
+const IMAGE_URL = '../../../../../assets/images/ModelType/diesel.png';
 
 function ModelType({ nextPage }) {
   const SectionProps = {
     type: TYPE,
+    url: IMAGE_URL,
     Info: <Info />,
     Pick: <Pick nextPage={nextPage} />,
   };
