097452d3117ea63283838e8030d3745bfad064f3
SyuA0529 <silbi3408@gmail.com>
2023-08-07 14:56:57 +0900

    feat: 모델 성능 조회 Mock API 테스트 코드 추가

--- backend/src/test/java/softeer/wantcar/cartalog/controller/ModelControllerTest.java
+++ backend/src/test/java/softeer/wantcar/cartalog/controller/ModelControllerTest.java
@@ -1,11 +1,14 @@
 package softeer.wantcar.cartalog.controller;
 
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import softeer.wantcar.cartalog.dto.HMGDataDto;
+import softeer.wantcar.cartalog.dto.ModelPerformanceDto;
 import softeer.wantcar.cartalog.dto.ModelTypeListResponseDto;
 
 import java.util.List;
@@ -14,8 +17,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("모델 도메인 컨트롤러 테스트")
 class ModelControllerTest {
+    SoftAssertions softAssertions;
     ModelController modelController = new ModelController();
     static String imageServerPath = "example-url";
+
+    @BeforeEach
+    void setUp() {
+        softAssertions = new SoftAssertions();
+    }
     
     @Nested
     @DisplayName("모델타입 목록 조회 테스트")
@@ -51,7 +60,7 @@ class ModelControllerTest {
         }
         
         @Test
-        @DisplayName("존재하지 않는 모델의 식별자를 조회할 경우 404 상태를 반환해야 한다.")
+        @DisplayName("존재하지 않는 모델의 식별자로 조회할 경우 404 상태를 반환해야 한다.")
         void returnStatusCode404WhenGetModelTypeByExistModelId() {
             //given
             //when
@@ -59,6 +68,59 @@ class ModelControllerTest {
 
             //then
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    @Nested
+    @DisplayName("모델 성능 조회 테스트")
+    class getModelPerformanceTest {
+        @Test
+        @DisplayName("올바른 요청시 200 상태와 함께 배기량과 평균연비를 반환해야 한다.")
+        void returnDisplacementAndFuelEfficiencyWithStatusCode200() {
+            //given
+            ModelPerformanceDto expectResponse = new ModelPerformanceDto(2199f, 12f);
+
+            //when
+            ModelPerformanceDto realResponse = modelController.getModelPerformance(1L, 3L, 5L).getBody();
+
+            //then
+            softAssertions.assertThat(realResponse.getDisplacement()).isEqualTo(2199f);
+            softAssertions.assertThat(realResponse.getFuelEfficiency()).isEqualTo(12f);
+            softAssertions.assertThat(realResponse).isEqualTo(expectResponse);
+            softAssertions.assertAll();
+        }
+
+        @Test
+        @DisplayName("존재하지 않는 모델의 식별자로 조회할 경우 404 에러를 반환해야 한다.")
+        void returnStatusCode404WhenGetNotExistModelId() {
+            //given
+            //when
+            ResponseEntity<ModelPerformanceDto> realResponse = modelController.getModelPerformance(-1L, 3L, 5L);
+
+            //then
+            assertThat(realResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        }
+
+        @Test
+        @DisplayName("존재하지 않는 파워트레인 식별자로 조회할 경우 404 에러를 반환해야 한다.")
+        void returnStatusCode404WhenGetNotExistPowerTrainId() {
+            //given
+            //when
+            ResponseEntity<ModelPerformanceDto> realResponse = modelController.getModelPerformance(1L, -1L, 5L);
+
+            //then
+            assertThat(realResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        }
+
+        @Test
+        @DisplayName("존재하지 않는 구동방식 식별자로 조회할 경우 404 에러를 반환해야 한다.")
+        void returnStatusCode404WhenGetNotExistWDId() {
+            //given
+            //when
+            ResponseEntity<ModelPerformanceDto> realResponse = modelController.getModelPerformance(1L, 3L, -1L);
+
+            //then
+            assertThat(realResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         }
     }
 
