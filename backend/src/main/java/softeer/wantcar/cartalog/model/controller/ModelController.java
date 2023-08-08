9bf74a931ffa7672c9bf1a049e678b39be0a3140
ijehyunpark <adam991217@naver.com>
2023-08-08 13:45:05 +0900

    fix: 모델 옵션 이름 수정

--- backend/src/main/java/softeer/wantcar/cartalog/model/controller/ModelController.java
+++ backend/src/main/java/softeer/wantcar/cartalog/model/controller/ModelController.java
@@ -82,7 +82,7 @@ public class ModelController {
                 .name("2WD")
                 .price(0)
                 .description("엔진에서 전달되는 동력이 전/후륜 바퀴 중 한쪽으로만 전달되어 차량을 움직이는 방식입니다\n" +
-                             "차체가 가벼워 연료 효율이 높습니다")
+                        "차체가 가벼워 연료 효율이 높습니다")
                 .imageUrl(imageServerPath + "/palisade/le-blanc/options/2wd.jpg")
                 .build();
     }
@@ -93,7 +93,7 @@ public class ModelController {
                 .name("4WD")
                 .price(2370000)
                 .description("전자식 상시 4륜 구동 시스템 입니다\n" +
-                             "도로의 상황이나 주행 환경에 맞춰 전후륜 구동력을 자동배분하여 주행 안전성을 높여줍니다")
+                        "도로의 상황이나 주행 환경에 맞춰 전후륜 구동력을 자동배분하여 주행 안전성을 높여줍니다")
                 .imageUrl(imageServerPath + "/palisade/le-blanc/options/4wd.jpg")
                 .build();
     }
@@ -101,7 +101,7 @@ public class ModelController {
     private ModelTypeListResponseDto.OptionDto getGasolineEngine() {
         return ModelTypeListResponseDto.PowerTrainOptionDto.builder()
                 .id(1L)
-                .name("디젤 2.2")
+                .name("가솔린 3.8")
                 .price(1480000)
                 .description("높은 토크로 파워풀한 드라이빙이 가능하며, 차급대비 연비 효율이 우수합니다")
                 .chosen(38)
@@ -114,11 +114,12 @@ public class ModelController {
 
     private ModelTypeListResponseDto.OptionDto getDieselEngine() {
         return ModelTypeListResponseDto.PowerTrainOptionDto.builder()
+                .id(2L)
                 .name("디젤 2.2")
                 .price(0)
                 .chosen(38)
                 .description("고마력의 우수한 가속 성능을 확보하여, 넉넉하고 안정감 있는 주행이 가능합니다\n" +
-                             "엔진의 진동이 적어 편안하고 조용한 드라이빙 감성을 제공합니다")
+                        "엔진의 진동이 적어 편안하고 조용한 드라이빙 감성을 제공합니다")
                 .imageUrl(imageServerPath + "/palisade/le-blanc/options/dieselengine2.2_s.jpg")
                 .powerTrainHMGData(List.of(
                         new ModelTypeListResponseDto.PowerTrainHMGDataDto("최고출력", 295f, "6,000", "PS/rpm"),
