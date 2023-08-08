b991dd19bdd35d5e59e2921e4a27a1b9e9cb96d0
jyh <jo990714@naver.com>
2023-08-08 23:59:00 +0900

    feat: 예산 범위 설정 toggle 기능 구현

--- Android/app/src/main/java/com/softeer/cartalog/ui/adapter/ButtonBindingAdapters.kt
+++ Android/app/src/main/java/com/softeer/cartalog/ui/adapter/ButtonBindingAdapters.kt
@@ -1,6 +1,9 @@
 package com.softeer.cartalog.ui.adapter
 
+import android.view.View
+import android.widget.ImageButton
 import androidx.appcompat.widget.AppCompatButton
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import com.softeer.cartalog.ui.activity.MainActivity
 
@@ -12,5 +15,31 @@ fun setOnClickTabBtn(
 ){
     button.setOnClickListener {
         activity.changeTab(idx)
+    }
+}
+
+@BindingAdapter("layout")
+fun setOnClickToggle(
+    button: ImageButton,
+    layout: ConstraintLayout
+){
+
+    button.setOnClickListener {
+        if(layout.visibility == View.VISIBLE){
+            layout.animate()
+                .alpha(0f)
+                .setDuration(300)
+                .withEndAction {
+                    layout.visibility = View.GONE
+                    layout.translationY = 0f
+                }
+            button.animate().rotation(0f).start()
+        } else {
+            layout.visibility = View.VISIBLE
+            layout.alpha = 0f
+            layout.animate()
+                .alpha(1f).duration = 300
+            button.animate().rotation(180f).start()
+        }
     }
 }
