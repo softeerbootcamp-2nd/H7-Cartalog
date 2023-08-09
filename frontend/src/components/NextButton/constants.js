ef316f314652450eb3c46a9600fcc3be25314437
afk0323 <jwv0323@naver.com>
2023-08-10 04:36:25 +0900

    feat: NextButton 컴포넌트 구현

---
+++ frontend/src/components/NextButton/constants.js
@@ -0,0 +1,16 @@
+export const TOTAL = {
+  PRICE: '현재 총 가격',
+  WON: '원',
+};
+
+export const BUTTON = {
+  TYPE: 'buttonD',
+  STATE: 'active',
+  MAIN_TITLE: '다음',
+};
+
+export const ROUND_BUTTON = {
+  TYPE: 'price',
+  STATE: 'active',
+  TITLE: '견적 요약',
+};
