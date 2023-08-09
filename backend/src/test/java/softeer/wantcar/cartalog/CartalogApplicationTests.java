f4bc39fcbc87b3923b9d8adb2594b1eb248ec596
SyuA0529 <silbi3408@gmail.com>
2023-08-09 11:17:01 +0900

    feat: 테스트 데이터베이스 설정 및 스키마 설정 완료

--- backend/src/test/java/softeer/wantcar/cartalog/CartalogApplicationTests.java
+++ backend/src/test/java/softeer/wantcar/cartalog/CartalogApplicationTests.java
@@ -1,9 +1,11 @@
 package softeer.wantcar.cartalog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 class CartalogApplicationTests {
     @Test
     void contextLoads() {
