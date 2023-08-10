1d2665f3faeeceeed118ac0b1a354dae2e22ed2a
afk0323 <jwv0323@naver.com>
2023-08-10 18:04:19 +0900

    feat: Header에 NavToggle 기능 추가

--- frontend/src/components/Header/Nav/style.jsx
+++ frontend/src/components/Header/Nav/style.jsx
@@ -12,12 +12,25 @@ export const Stage = styled.ul`
   gap: 40px;
 `;
 
-export const Step = styled.li`
+export const Step = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Text = styled.li`
   display: flex;
   align-items: center;
   justify-content: center;
   width: 52px;
   height: 22px;
   color: ${({ theme }) => theme.color.gray['200']};
-  font: ${({ theme }) => theme.font.textKR.Medium14};
+  font: ${({ theme }) => theme.font.headKR.Medium14};
+
+  &.selected {
+    color: ${({ theme }) => theme.color.primary.default};
+  }
+`;
+
+export const Bar = styled.div`
+  margin: 6px 17px -8px 17px;
 `;
---
+++ frontend/src/components/Header/constants.js
@@ -0,0 +1,16 @@
+export const EXIT = {
+  TEXT: '종료',
+};
+
+export const LOGO = {
+  TYPE: '펠리세이드',
+};
+
+export const NAV = [
+  { to: '/', label: '트림', page: 1 },
+  { to: '/modelType', label: '타입', page: 2 },
+  { to: '/exteriorColor', label: '외장', page: 3 },
+  { to: '/interiorColor', label: '내장', page: 4 },
+  { to: '/optionPicker', label: '옵션', page: 5 },
+  { to: '/estimation', label: '완료', page: 6 },
+];
