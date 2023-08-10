f37193f09c1ccde9ddd820e47b7c902d3337ff97
afk0323 <jwv0323@naver.com>
2023-08-10 16:00:02 +0900

    feat: 내장색상 페이지 API 연동

--- frontend/src/pages/InteriorColorPage/index.jsx
+++ frontend/src/pages/InteriorColorPage/index.jsx
@@ -1,19 +1,42 @@
+import { useState, useEffect } from 'react';
+import { useData } from '../../utils/Context';
+import { INTERIOR_COLOR } from './constants';
 import Section from '../../components/Section';
 import Info from './Info';
 import Pick from './Pick';
 
-const TYPE = 'InteriorColor';
-const IMAGE_URL = '../../../../../assets/images/TrimSelect/interior.png';
-
+// !FIX 내장색상 API는 아직 완성 전
 function InteriorColor() {
+  const [isFetched, setIsFetched] = useState(false);
+  const { setTrimState, trimId, interiorColor } = useData();
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `http://3.36.126.30/models/trims/interior-colors?exteriorColorId=1&trimId=${trimId}`,
+      );
+      const dataFetch = await response.json();
+      setTrimState((prevState) => ({
+        ...prevState,
+        page: 4,
+        interiorColor: {
+          ...prevState.interiorColor,
+          dataFetch: [...dataFetch.trimInteriorColorDtoList],
+        },
+      }));
+      setIsFetched(true);
+    }
+    fetchData();
+  }, []);
+
   const SectionProps = {
-    type: TYPE,
-    url: IMAGE_URL,
+    type: INTERIOR_COLOR.TYPE,
+    url: interiorColor.pickCarImageUrl,
     Info: <Info />,
     Pick: <Pick />,
   };
 
-  return <Section {...SectionProps} />;
+  return isFetched ? <Section {...SectionProps} /> : <>Loding</>;
 }
 
 export default InteriorColor;
