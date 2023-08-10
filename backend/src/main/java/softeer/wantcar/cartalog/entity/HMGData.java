7dca29c3f4e6ea4aab691365e238ed61f9fea064
ijehyunpark <adam991217@naver.com>
2023-08-10 17:29:46 +0900

    refactor: 사용하지 않는 lombook annotation 삭제

--- backend/src/main/java/softeer/wantcar/cartalog/entity/HMGData.java
+++ backend/src/main/java/softeer/wantcar/cartalog/entity/HMGData.java
@@ -1,12 +1,5 @@
 package softeer.wantcar.cartalog.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.ToString;
-
-@Builder
-@AllArgsConstructor
-@ToString
 public class HMGData {
     private Long id;
     private String name;
