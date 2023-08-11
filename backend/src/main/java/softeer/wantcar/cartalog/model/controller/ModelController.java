e539680bb6e61bcfcd66b33c7dbe76e96fb57398
SyuA0529 <silbi3408@gmail.com>
2023-08-11 23:54:19 +0900

    feat: 더이상 사용되지 않는 Mock API 및 DTO 삭제

--- backend/src/main/java/softeer/wantcar/cartalog/model/controller/ModelController.java
+++ backend/src/main/java/softeer/wantcar/cartalog/model/controller/ModelController.java
@@ -9,10 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import softeer.wantcar.cartalog.model.dto.ModelPerformanceDto;
 import softeer.wantcar.cartalog.model.dto.ModelTypeListResponseDto;
 import softeer.wantcar.cartalog.model.repository.ModelOptionQueryRepository;
 
@@ -42,16 +40,5 @@ public class ModelController {
         }
 
         return new ResponseEntity<>(dto, HttpStatus.OK);
-    }
-
-    @GetMapping("/{modelId}/performance")
-    public ResponseEntity<ModelPerformanceDto> getModelPerformance(@PathVariable("modelId") Long modelId,
-                                                                   @PathParam("powerTrainId") Long powerTrainId,
-                                                                   @PathParam("WDId") Long wdId) {
-        if (modelId < 0 || powerTrainId < 0 || wdId < 0) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        ModelPerformanceDto modelPerformance = new ModelPerformanceDto(2199, 12);
-        return new ResponseEntity<>(modelPerformance, HttpStatus.OK);
     }
 }
