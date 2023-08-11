c60a39387c3640605424b0c64ccc3358de6ebf19
SyuA0529 <silbi3408@gmail.com>
2023-08-11 22:44:34 +0900

    refactor: 사용되지 않는 import문 최적화

--- backend/src/test/java/softeer/wantcar/cartalog/model/repository/ModelOptionQueryRepositoryImplTest.java
+++ backend/src/test/java/softeer/wantcar/cartalog/model/repository/ModelOptionQueryRepositoryImplTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 import softeer.wantcar.cartalog.model.dto.ModelTypeListResponseDto;
