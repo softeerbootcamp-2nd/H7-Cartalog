2a024df07ffa6933df4e7048c52d36e9be348850
afk0323 <jwv0323@naver.com>
2023-08-12 21:10:29 +0900

    refactor: Context 코드 리팩토링

--- frontend/src/utils/Context/index.jsx
+++ frontend/src/utils/Context/index.jsx
@@ -14,58 +14,62 @@ export function StateProvider({ children }) {
   ];
 
   const initialState = stateKeys.reduce((acc, key) => {
-    if (key === 'price') {
-      acc[key] = {
-        trimPrice: 0,
-        powerTrainPrice: 0,
-        bodyTypePrice: 0,
-        wheelDrivePrice: 0,
-        exteriorColorPrice: 0,
-        interiorColorPrice: 0,
-        optionPrice: 0,
-      };
-    } else if (key === 'trim') {
-      acc[key] = {
-        trimFetch: [],
-        isTrimFetch: false,
-        trimId: 2,
-      };
-    } else if (key === 'modelType') {
-      acc[key] = {
-        modelTypeFetch: [],
-        isModelTypeFetch: false,
-        pickId: 1,
-        powerTrainType: '파워트레인/성능',
-        bodyTypeType: '바디타입',
-        wheelDriveType: '구동방식',
-        powerTrainId: null,
-        bodyTypeId: null,
-        wheelDriveId: null,
-        powerTrainOption: null,
-        bodyTypeOption: null,
-        wheelDriveOption: null,
-      };
-    } else if (key === 'exteriorColor') {
-      acc[key] = {
-        dataFetch: [],
-        exteriorColorId: 'A2B',
-
-        pickName: '어비스 블랙 펄',
-        pickCarImageUrl: 'https://want-car-image.s3.ap-northeast-2.amazonaws.com/palisade',
-        rotate: false,
-      };
-    } else if (key === 'interiorColor') {
-      acc[key] = {
-        dataFetch: [],
-        interiorColorId: 'I49',
-
-        pick: 'A22',
-        pickName: '퀼팅천연(블랙)',
-        pickCarImageUrl:
-          'https://want-car-image.s3.ap-northeast-2.amazonaws.com/palisade/le-blanc/options/10_driverseat_s.jpg',
-      };
-    } else {
-      acc[key] = null; // 다른 키들은 null로 초기화
+    switch (key) {
+      case 'price':
+        acc[key] = {
+          trimPrice: 0,
+          powerTrainPrice: 0,
+          bodyTypePrice: 0,
+          wheelDrivePrice: 0,
+          exteriorColorPrice: 0,
+          interiorColorPrice: 0,
+          optionPrice: 0,
+        };
+        break;
+      case 'trim':
+        acc[key] = {
+          trimFetch: [],
+          isTrimFetch: false,
+          trimId: 2,
+        };
+        break;
+      case 'modelType':
+        acc[key] = {
+          modelTypeFetch: [],
+          isModelTypeFetch: false,
+          pickId: 1,
+          powerTrainType: '파워트레인/성능',
+          bodyTypeType: '바디타입',
+          wheelDriveType: '구동방식',
+          powerTrainId: null,
+          bodyTypeId: null,
+          wheelDriveId: null,
+          powerTrainOption: null,
+          bodyTypeOption: null,
+          wheelDriveOption: null,
+        };
+        break;
+      case 'exteriorColor':
+        acc[key] = {
+          dataFetch: [],
+          exteriorColorId: 'A2B',
+          pickName: '어비스 블랙 펄',
+          pickCarImageUrl: 'https://want-car-image.s3.ap-northeast-2.amazonaws.com/palisade',
+          rotate: false,
+        };
+        break;
+      case 'interiorColor':
+        acc[key] = {
+          dataFetch: [],
+          interiorColorId: 'I49',
+          pick: 'A22',
+          pickName: '퀼팅천연(블랙)',
+          pickCarImageUrl:
+            'https://want-car-image.s3.ap-northeast-2.amazonaws.com/palisade/le-blanc/options/10_driverseat_s.jpg',
+        };
+        break;
+      default:
+        acc[key] = null; // 다른 키들은 null로 초기화
     }
     return acc;
   }, {});
