7c992914ba5963a8baa14f54269b07c3cce858da
afk0323 <jwv0323@naver.com>
2023-08-13 02:20:30 +0900

    fix: 자동차 위치 fixed로 고정

--- frontend/src/pages/ExteriorColorPage/Info/TrimImage/style.jsx
+++ frontend/src/pages/ExteriorColorPage/Info/TrimImage/style.jsx
@@ -1,12 +1,12 @@
 import { styled } from 'styled-components';
 
 export const TrimImage = styled.div`
-  position: relative;
+  position: fixed;
   display: flex;
   width: 592px;
   height: 325px;
   margin-top: 36px;
-  margin-left: 65px;
+  margin-left: 220px;
 `;
 
 export const RotateImage = styled.img`
