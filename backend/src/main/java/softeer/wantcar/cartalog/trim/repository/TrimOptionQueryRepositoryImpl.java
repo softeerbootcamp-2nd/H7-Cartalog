14f374f067395cadb25344fc5458492457c00d81
SyuA0529 <silbi3408@gmail.com>
2023-08-14 10:31:41 +0900

    refactor: 사용되지 않는 import문 삭제

--- backend/src/main/java/softeer/wantcar/cartalog/trim/repository/TrimOptionQueryRepositoryImpl.java
+++ backend/src/main/java/softeer/wantcar/cartalog/trim/repository/TrimOptionQueryRepositoryImpl.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -13,7 +12,10 @@ import softeer.wantcar.cartalog.global.ServerPath;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Repository
