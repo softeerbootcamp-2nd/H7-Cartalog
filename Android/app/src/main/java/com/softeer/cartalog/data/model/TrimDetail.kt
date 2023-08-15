b83f1f638af3e6f0efeae6c2a9121776b352f71d
Glacier Han <61905052+Glacier-Han@users.noreply.github.com>
2023-08-15 17:42:41 +0900

    feat: TypeFragment내 HMG DATA 표시를 위한 trim detail api 기반작업

---
+++ Android/app/src/main/java/com/softeer/cartalog/data/model/TrimDetail.kt
@@ -0,0 +1,7 @@
+package com.softeer.cartalog.data.model
+
+data class TrimDetail(
+    val detailTrimId: Int,
+    val displacement: Int,
+    val fuelEfficiency: Float
+)
--- Android/app/src/main/java/com/softeer/cartalog/data/remote/api/CarApi.kt
+++ Android/app/src/main/java/com/softeer/cartalog/data/remote/api/CarApi.kt
@@ -1,5 +1,6 @@
 package com.softeer.cartalog.data.remote.api
 
+import com.softeer.cartalog.data.model.TrimDetail
 import com.softeer.cartalog.data.model.Trims
 import com.softeer.cartalog.data.model.Types
 import retrofit2.Response
@@ -10,6 +11,12 @@ interface CarApi {
 
     @GET("models/types")
     suspend fun getTypes(@Query("basicModelId") basicModelId: Int): Response<Types>
+
+    @GET("/models/trims/detail")
+    suspend fun getTrimsDetail(
+        @Query("modelTypeIds") modelTypeIds: String,
+        @Query("trimId") trimId: Int
+    ): Response<TrimDetail>
 
     @GET("/models/trims")
     suspend fun getTrims(@Query("basicModelId") basicModelId: Int): Response<Trims>
