447cc85d21a5c904737acc6532f6188a2b37022d
jyh <jo990714@naver.com>
2023-08-16 15:06:54 +0900

    feat: Repository에 Local DB 연결 추가

--- Android/app/src/main/java/com/softeer/cartalog/data/repository/CarRepositoryImpl.kt
+++ Android/app/src/main/java/com/softeer/cartalog/data/repository/CarRepositoryImpl.kt
@@ -1,6 +1,10 @@
 package com.softeer.cartalog.data.repository
 
+import com.softeer.cartalog.data.enums.PriceDataType
+import com.softeer.cartalog.data.model.MyCar
+import com.softeer.cartalog.data.model.PriceData
 import com.softeer.cartalog.data.model.Trim
+import com.softeer.cartalog.data.model.Trims
 import com.softeer.cartalog.data.repository.local.CarLocalDataSource
 import com.softeer.cartalog.data.repository.remote.CarRemoteDataSource
 
@@ -9,8 +13,30 @@ class CarRepositoryImpl(
     private val carRemoteDataSource: CarRemoteDataSource
 ) : CarRepository {
 
-    override suspend fun getTrims(): ArrayList<Trim> {
+    override suspend fun getTrims(): Trims {
         val response = carRemoteDataSource.getTrims()
-        return if (response.isSuccessful) response.body()!!.trims else arrayListOf()
+        return if (response.isSuccessful) response.body()!! else Trims("", arrayListOf())
     }
+
+    override suspend fun setInitialMyCarData(carName: String, trim: Trim) {
+
+        val myCar = MyCar(carName, trim.name, trim.id, trim.exteriorImageUrl, trim.interiorImageUrl)
+        val carId = carLocalDataSource.setInitialMyCar(myCar)
+
+        val priceDataList = mutableListOf<PriceData>()
+        trim.defaultInfo?.modelTypes?.forEach {
+            priceDataList.add(
+                PriceData(
+                    carId,
+                    PriceDataType.fromTypeName(it.type),
+                    it.option.id,
+                    it.option.name,
+                    it.option.price,
+                    null
+                )
+            )
+        }
+        carLocalDataSource.setInitialPriceData(priceDataList.toList())
+    }
+
 }
--- Android/app/src/main/java/com/softeer/cartalog/data/repository/local/CarLocalDataSource.kt
+++ Android/app/src/main/java/com/softeer/cartalog/data/repository/local/CarLocalDataSource.kt
@@ -1,4 +1,20 @@
 package com.softeer.cartalog.data.repository.local
 
-class CarLocalDataSource {
+import com.softeer.cartalog.data.local.MyCarDatabase
+import com.softeer.cartalog.data.model.MyCar
+import com.softeer.cartalog.data.model.PriceData
+
+class CarLocalDataSource(
+    private val myCarDb: MyCarDatabase
+) {
+    private val myCarDao = myCarDb.myCarDao()
+    private val priceDataDao = myCarDb.priceDataDao()
+
+    suspend fun setInitialMyCar(input: MyCar): Int {
+        return myCarDao.insertMyCar(input).toInt()
+    }
+
+    suspend fun setInitialPriceData(inputs: List<PriceData>) {
+        priceDataDao.insertPriceDataList(inputs)
+    }
 }
