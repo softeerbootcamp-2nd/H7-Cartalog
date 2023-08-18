0003d38ed831e6a1960168c1de5b8bddce37a364
ijehyunpark <adam991217@naver.com>
2023-08-19 00:42:32 +0900

    docs: 견적서 조회, 추가 swagger 명세 추가

--- backend/src/main/java/softeer/wantcar/cartalog/estimate/controller/EstimateController.java
+++ backend/src/main/java/softeer/wantcar/cartalog/estimate/controller/EstimateController.java
@@ -1,6 +1,6 @@
 package softeer.wantcar.cartalog.estimate.controller;
 
-import io.swagger.annotations.Api;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +19,18 @@ import softeer.wantcar.cartalog.estimate.service.EstimateService;
 public class EstimateController {
     private final EstimateService estimateService;
 
+    @ApiOperation(
+            value = "견적서 식별자 조회",
+            notes = "견적서 식별자를 조회한다. 만약 없을 경우 견적서를 생성하고 해당 식별자를 조회한다. 선택 옵션, 패키지는 다음과 같은 입력을 받습니다. 옵션: O#, 패키지: P#")
+    @ApiImplicitParam(
+            name = "estimateRequestDto", value = "견적서 조합", required = true, dataTypeClass = EstimateRequestDto.class, paramType = "body",
+            example = "{\n  \"detailTrimId\": 9,\n  \"exteriorColorCode\": \"A2B\",\n  \"interiorColorCode\": \"I49\",\n  \"selectOptionOrPackageIds\": [\n  ]\n}")
+    @ApiResponses({
+            @ApiResponse(code = 400, message = "불가능한 조합입니다."),
+    })
     @PostMapping("")
     public ResponseEntity<Long> registerOrGetEstimate(@RequestBody EstimateRequestDto estimateRequestDto) {
-        try{
+        try {
             Long estimateId = estimateService.saveOrFindEstimateId(estimateRequestDto);
             log.info(String.valueOf(estimateId));
             return ResponseEntity.ok().body(estimateId);
