65705340784b42311f9c9bf027ab446c25921abb
ijehyunpark <adam991217@naver.com>
2023-08-19 14:16:59 +0900

    docs: 트림 평균 출고 가격 조히 관련 swagger 명세 추가

--- backend/src/main/java/softeer/wantcar/cartalog/estimate/controller/EstimateController.java
+++ backend/src/main/java/softeer/wantcar/cartalog/estimate/controller/EstimateController.java
@@ -1,6 +1,9 @@
 package softeer.wantcar.cartalog.estimate.controller;
 
-import io.swagger.annotations.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,13 @@ import softeer.wantcar.cartalog.estimate.repository.EstimateQueryRepository;
 public class EstimateController {
     private final EstimateQueryRepository estimateQueryRepository;
 
+    @ApiOperation(
+            value = "트림 평균 출고 가격 조회",
+            notes = "실제 출고 견적에서 특정 트림의 평균 출고 가격을 조회합니다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = "trimId", value = "트림 식별자", required = true,
+                    dataTypeClass = Long.class, paramType = "query", example = "2")})
     @GetMapping("distribution")
     public ResponseEntity<Long> getDistribution(@RequestParam("trimId") Long trimId) {
         return ResponseEntity.ok(estimateQueryRepository.findAveragePrice(trimId));
