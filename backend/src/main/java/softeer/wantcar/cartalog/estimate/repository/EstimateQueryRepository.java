bb3c333dcde0e83b9ea6d2363243fe0e85210173
SyuA0529 <silbi3408@gmail.com>
2023-08-19 15:14:19 +0900

    feat: EstimateQueryRepository의 견적 정보 조회 구현

---
+++ backend/src/main/java/softeer/wantcar/cartalog/estimate/repository/EstimateQueryRepository.java
@@ -0,0 +1,19 @@
+package softeer.wantcar.cartalog.estimate.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import softeer.wantcar.cartalog.estimate.repository.dto.EstimateOptionListDto;
+
+public interface EstimateQueryRepository {
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    class EstimateOptionIdsResult {
+        private Long trimId;
+        private Long optionId;
+        private Long packageId;
+    }
+
+    EstimateOptionListDto findEstimateOptionIdsByEstimateId(Long estimateId);
+}
---
+++ backend/src/main/java/softeer/wantcar/cartalog/estimate/repository/EstimateQueryRepositoryImpl.java
@@ -0,0 +1,44 @@
+package softeer.wantcar.cartalog.estimate.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import softeer.wantcar.cartalog.estimate.repository.dto.EstimateOptionListDto;
+import softeer.wantcar.cartalog.global.utils.RowMapperUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class EstimateQueryRepositoryImpl implements EstimateQueryRepository {
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    @Override
+    public EstimateOptionListDto findEstimateOptionIdsByEstimateId(Long estimateId) {
+        SqlParameterSource parameters = new MapSqlParameterSource()
+                .addValue("estimateId", estimateId);
+        List<EstimateOptionIdsResult> queryResult = jdbcTemplate.query(QueryString.findEstimateOptionIdsByEstimateId,
+                parameters, RowMapperUtils.mapping(EstimateOptionIdsResult.class));
+        if(queryResult.isEmpty()) {
+            return null;
+        }
+
+        List<Long> optionIds = queryResult.stream()
+                .map(EstimateOptionIdsResult::getOptionId)
+                .filter(id -> id > 0)
+                .distinct()
+                .collect(Collectors.toList());
+        List<Long> packageIds = queryResult.stream()
+                .map(EstimateOptionIdsResult::getPackageId)
+                .filter(id -> id > 0)
+                .distinct()
+                .collect(Collectors.toList());
+
+        return new EstimateOptionListDto(queryResult.get(0).getTrimId(), optionIds, packageIds);
+    }
+}
