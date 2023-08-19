07b2a6536364080821dd23859cedceb93d6a7cc3
SyuA0529 <silbi3408@gmail.com>
2023-08-19 20:45:31 +0900

    feat: SimilarityService의 유사 견적 개수 조회 테스트 추가

---
+++ backend/src/main/java/softeer/wantcar/cartalog/estimate/dto/SimilarEstimateCountResponseDto.java
@@ -0,0 +1,14 @@
+package softeer.wantcar.cartalog.estimate.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import softeer.wantcar.cartalog.estimate.repository.dto.EstimateCountDto;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class SimilarEstimateCountResponseDto {
+    private Long myEstimateCount;
+    private List<EstimateCountDto> similarEstimateCounts;
+}
