72b1dfe154c94d93d977f8e9220a52bfbff96ad8
SyuA0529 <silbi3408@gmail.com>
2023-08-19 23:32:23 +0900

    refactor: import문 최적화

--- backend/src/test/java/softeer/wantcar/cartalog/estimate/service/SimilarityServiceTest.java
+++ backend/src/test/java/softeer/wantcar/cartalog/estimate/service/SimilarityServiceTest.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
