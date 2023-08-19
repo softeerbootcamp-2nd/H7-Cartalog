71a07e2851dce7465bd88e253d4088f8f3b3cbd4
cocoa <jwv0323@naver.com>
2023-08-20 04:06:13 +0900

    refactor: 견적완료 페이지 Footer 하단 고정

--- frontend/src/pages/EstimationPage/Footer/style.jsx
+++ frontend/src/pages/EstimationPage/Footer/style.jsx
@@ -1,17 +1,49 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { EASE_OUT_CUBIC } from '../../../constants';
+
+const slideCss = css`
+  transform: translateY(120%);
+  transition: transform 0.5s ${EASE_OUT_CUBIC};
+
+  &.visible {
+    transform: translateY(0);
+  }
+`;
 
 export const Footer = styled.div`
+  ${slideCss}
+  width: 100%;
   display: flex;
   flex-direction: column;
-  width: 1024px;
-  margin: 0 auto;
-  margin-bottom: 16px;
+  position: fixed;
+  bottom: 0;
+  z-index: 1;
 `;
 
 export const ImageViewButtonWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
-  margin-bottom: 25px;
+  width: 1280px;
+  padding: 0 128px;
+  margin: 0 auto;
+  margin-bottom: 15px;
+  box-sizing: border-box;
+`;
+
+export const FooterBlur = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 114px;
+  background: rgba(255, 255, 255, 0.9);
+  backdrop-filter: blur(6px);
+`;
+
+export const FooterWrapper = styled.div`
+  width: 1280px;
+  margin: 0 auto;
+  padding: 0 128px;
+  box-sizing: border-box;
 `;
 
 export const ButtonWrapper = styled.div`
