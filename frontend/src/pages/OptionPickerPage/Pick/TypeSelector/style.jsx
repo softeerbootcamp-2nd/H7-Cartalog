13ede18753c20f97c6e46829aa4ef515bfb053e9
Jo <tockiib@gmail.com>
2023-08-20 00:15:54 +0900

    feat: 옵션 선택 페이지 기본/추가옵션 인디케이터 추가

--- frontend/src/pages/OptionPickerPage/Pick/TypeSelector/style.jsx
+++ frontend/src/pages/OptionPickerPage/Pick/TypeSelector/style.jsx
@@ -1,8 +1,23 @@
 import { styled } from 'styled-components';
 
 export const TypeSelector = styled.nav`
+  position: relative;
   display: flex;
   gap: 24px;
+  width: fit-content;
+  padding-bottom: 2px;
+`;
+
+export const Bar = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 18px;
+  height: 2px;
+  border-radius: 1px;
+  background-color: ${({ theme }) => theme.color.primary.default};
+  transition: transform 0.2s ease;
+  transform-origin: left center;
 `;
 
 export const SelectorItem = styled.button`
