d98181c3201701fc7cffd79b408be11e8517dca8
SyuA0529 <silbi3408@gmail.com>
2023-08-19 20:51:15 +0900

    feat: SimilarityController 유사 견적 개수 조회 API 테스트 추가

--- backend/src/main/java/softeer/wantcar/cartalog/estimate/dto/SimilarEstimateCountResponseDto.java
+++ backend/src/main/java/softeer/wantcar/cartalog/estimate/dto/SimilarEstimateCountResponseDto.java
@@ -1,5 +1,6 @@
 package softeer.wantcar.cartalog.estimate.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import softeer.wantcar.cartalog.estimate.repository.dto.EstimateCountDto;
@@ -8,6 +9,7 @@ import java.util.List;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class SimilarEstimateCountResponseDto {
     private Long myEstimateCount;
     private List<EstimateCountDto> similarEstimateCounts;
