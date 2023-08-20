98ad42d44f6270168070bc14635ea896d5f52790
SyuA0529 <silbi3408@gmail.com>
2023-08-20 19:01:02 +0900

    feat: 변경된 지연된 계산 로직을 위해 HashTagMap에 index 추가 및 이름 변경

--- backend/src/main/java/softeer/wantcar/cartalog/estimate/repository/SimilarityQueryRepository.java
+++ backend/src/main/java/softeer/wantcar/cartalog/estimate/repository/SimilarityQueryRepository.java
@@ -1,11 +1,11 @@
 package softeer.wantcar.cartalog.estimate.repository;
 
-import softeer.wantcar.cartalog.estimate.repository.dto.HashTagMap;
+import softeer.wantcar.cartalog.estimate.repository.dto.PendingHashTagMap;
 
 import java.util.List;
 
 public interface SimilarityQueryRepository {
-    List<HashTagMap> findPendingHashTagMapByTrimIdAndHashTagKey(Long trimId, String hashTagKey);
+    List<PendingHashTagMap> findPendingHashTagMapByTrimIdAndHashTagKey(Long trimId, String hashTagKey);
 
     List<String> findSimilarHashTagKeysByTrimIdAndHashTagKey(Long trimId, String hashTagKey);
 
