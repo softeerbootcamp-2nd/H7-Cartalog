9ff4f8cea3e40a0feb012b6a39ab4557d3838563
SyuA0529 <silbi3408@gmail.com>
2023-08-20 23:14:24 +0900

    feat: 변경된 유사도 계산 로직에 필요한 조회문 추가

--- backend/src/main/java/softeer/wantcar/cartalog/estimate/repository/SimilarityCommandRepository.java
+++ backend/src/main/java/softeer/wantcar/cartalog/estimate/repository/SimilarityCommandRepository.java
@@ -1,4 +1,15 @@
 package softeer.wantcar.cartalog.estimate.repository;
 
+import softeer.wantcar.cartalog.estimate.repository.dto.SimilarityInfo;
+
+import java.util.List;
+
 public interface SimilarityCommandRepository {
+    void updateLastCalculatedIndex(Long trimId, String hashTagKey, long lastIndex);
+
+    void deleteSimilarities(Long trimId, String hashTagKey);
+
+    void saveSimilarities(Long trimId, String hashTagKey, List<SimilarityInfo> similarityInfos);
+
+    void saveHashTagKey(Long trimId, String hashTagKey, long lastIndex);
 }
