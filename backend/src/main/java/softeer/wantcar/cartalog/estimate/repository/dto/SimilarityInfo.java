738e6b211c96ce3b5b9fce0d5a20e2d905040bcb
SyuA0529 <silbi3408@gmail.com>
2023-08-20 19:17:30 +0900

    refactor: 유사도 계산 로직을 위해 SimilarityInfo에 index 추가

--- backend/src/main/java/softeer/wantcar/cartalog/estimate/repository/dto/SimilarityInfo.java
+++ backend/src/main/java/softeer/wantcar/cartalog/estimate/repository/dto/SimilarityInfo.java
@@ -1,13 +1,15 @@
 package softeer.wantcar.cartalog.estimate.repository.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 @EqualsAndHashCode
+@Builder
 public class SimilarityInfo {
+    private long index;
     private String hashTagKey;
     private Double similarity;
 }
