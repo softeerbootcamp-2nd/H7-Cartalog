f0029799330062bfad7e94747f374fa8516630e9
ijehyunpark <adam991217@naver.com>
2023-08-20 16:53:51 +0900

    feat: 견적서 조회 서비스 구현

--- backend/src/main/java/softeer/wantcar/cartalog/estimate/service/EstimateService.java
+++ backend/src/main/java/softeer/wantcar/cartalog/estimate/service/EstimateService.java
@@ -1,7 +1,10 @@
 package softeer.wantcar.cartalog.estimate.service;
 
 import softeer.wantcar.cartalog.estimate.dto.EstimateRequestDto;
+import softeer.wantcar.cartalog.estimate.dto.EstimateResponseDto;
 
 public interface EstimateService {
     Long saveOrFindEstimateId(EstimateRequestDto estimateRequestDto);
+
+    EstimateResponseDto findEstimateByEstimateId(Long estimateId);
 }
