0ddad17add5bcb59590ecbb976f3e9847e9689ce
cocoa <jwv0323@naver.com>
2023-08-21 11:17:44 +0900

    feat: 완료페이지 HMGArea 데이터 수정

--- frontend/src/pages/EstimationPage/HMGArea/index.jsx
+++ frontend/src/pages/EstimationPage/HMGArea/index.jsx
@@ -2,11 +2,39 @@ import PriceCard from './PriceCard';
 import SimilarCard from './SimilarCard';
 import * as S from './style';
 
-function HMGArea({ data }) {
+function HMGArea() {
+  const MOCK_HMGDATA = {
+    trim: '르블랑',
+    price: {
+      min: 43460000,
+      max: 47650000,
+      current: 45800000,
+    },
+    value: 2500,
+    similar: [
+      {
+        id: 1,
+        value: 3422,
+      },
+      {
+        id: 2,
+        value: 3200,
+      },
+      {
+        id: 3,
+        value: 3000,
+      },
+      {
+        id: 4,
+        value: 2800,
+      },
+    ],
+  };
+
   return (
     <S.HMGArea>
-      <PriceCard data={data} />
-      <SimilarCard data={data} />
+      <PriceCard data={MOCK_HMGDATA} />
+      <SimilarCard data={MOCK_HMGDATA} />
     </S.HMGArea>
   );
 }
--- frontend/src/pages/EstimationPage/index.jsx
+++ frontend/src/pages/EstimationPage/index.jsx
@@ -8,34 +8,6 @@ import Detail from './Detail';
 import HMGArea from './HMGArea';
 import Footer from './Footer';
 
-const MOCK_HMGDATA = {
-  trim: '르블랑',
-  price: {
-    min: 43460000,
-    max: 47650000,
-    current: 45800000,
-  },
-  value: 2500,
-  similar: [
-    {
-      id: 1,
-      value: 3422,
-    },
-    {
-      id: 2,
-      value: 3200,
-    },
-    {
-      id: 3,
-      value: 3000,
-    },
-    {
-      id: 4,
-      value: 2800,
-    },
-  ],
-};
-
 function Estimation() {
   const data = useData();
   const SelectModel = data.trim.fetchData.find((model) => model.id === data.trim.id);
@@ -63,7 +35,7 @@ function Estimation() {
         <Info />
         <S.PageContents>
           <Detail />
-          <HMGArea data={MOCK_HMGDATA} />
+          <HMGArea />
         </S.PageContents>
         <Footer />
       </S.Estimation>
