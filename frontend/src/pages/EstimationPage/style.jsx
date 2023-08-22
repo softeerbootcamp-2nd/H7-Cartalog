bd7e95d1a92be3edf818ba5821c08c0af94536eb
Jo <tockiib@gmail.com>
2023-08-23 01:14:09 +0900

    feat: 완료 페이지 시작 애니메이션 추가

--- frontend/src/pages/EstimationPage/style.jsx
+++ frontend/src/pages/EstimationPage/style.jsx
@@ -1,4 +1,5 @@
 import { styled } from 'styled-components';
+import { SlideFromBottom } from '../../styles/GlobalStyle';
 
 export const Estimation = styled.div`
   min-width: max(1280px, 100%);
@@ -18,11 +19,17 @@ export const PDF = styled.div`
   }
 `;
 
+export const Info = styled.div`
+  ${SlideFromBottom}
+  animation-delay: 2.5s;
+`;
+
 export const PageContents = styled.div`
   display: flex;
   width: 1024px;
   margin: 0 auto;
   gap: 70px;
+  margin-bottom: 200px;
 
   @media print {
     padding: 0 69px;
