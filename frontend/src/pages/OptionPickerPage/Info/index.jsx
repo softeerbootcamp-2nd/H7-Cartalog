ecbad90e979e850eae946e1f877af08ceef4baaa
Jo <tockiib@gmail.com>
2023-08-23 15:22:06 +0900

    feat: 옵션 페이지 이미지 접기/펼치기 버튼 추가

--- frontend/src/pages/OptionPickerPage/Info/index.jsx
+++ frontend/src/pages/OptionPickerPage/Info/index.jsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from 'react';
 import * as S from './style';
 import Title from '../../../components/Title';
 import Description from './Description';
+import ImageViewButton from '../../../components/ImageViewButton';
 
 const TYPE = 'dark';
 const CATEGORY = '파워트레인/성능';
 
-function Info({ optionId }) {
+function Info({ optionId, expanded, setExpanded }) {
   const [optionInfo, setOptionInfo] = useState();
   const [imageUrl, setImageUrl] = useState();
+  const imageButtonText = expanded ? '이미지 접기' : '이미지 확인';
   const TitleProps = {
     type: TYPE,
     subTitle: optionInfo?.category ?? CATEGORY,
@@ -37,6 +39,7 @@ function Info({ optionId }) {
         <Title {...TitleProps} />
       </S.ModelText>
       <S.ModelImage src={imageUrl} />
+      <ImageViewButton text={imageButtonText} onClick={() => setExpanded((prev) => !prev)} />
     </S.Info>
   );
 }
