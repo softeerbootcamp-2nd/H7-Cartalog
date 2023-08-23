2b04bfd17a5a395c90704cf7b945003a7e6bcfc8
cocoa <jwv0323@naver.com>
2023-08-23 10:13:17 +0900

    feat: 유사 출고 견적 API 연동

--- frontend/src/pages/EstimationPage/HMGArea/SimilarCard/index.jsx
+++ frontend/src/pages/EstimationPage/HMGArea/SimilarCard/index.jsx
@@ -1,11 +1,13 @@
 import { useState, useRef } from 'react';
+import { useData } from '../../../../utils/Context';
 import * as S from './style';
 import Chart from '../../../../components/Chart';
 import HMGCard from '../../../../components/HMGCard';
 import useIntersectionObserver from '../../../../hooks/useIntersectionObserver';
 import SimilarPopup from '../SimilarPopup';
 
-function SimilarCard({ data }) {
+function SimilarCard() {
+  const data = useData();
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
@@ -27,11 +29,15 @@ function SimilarCard({ data }) {
       <S.ChartWrapper
         className={isInViewport ? null : 'stop'}
         ref={targetRef}
-        $n={(data?.similar?.length ?? 0) + 1}
+        $n={(data.estimation.similarEstimateCounts?.length ?? 0) + 1}
       >
-        <Chart active value={data?.value} max={max} />
-        {data?.similar?.map((item) => (
-          <Chart key={item.id} value={item.value} max={max} />
+        <Chart
+          active
+          value={data.estimation.myEstimateCount}
+          max={Math.max(data.estimation.myEstimateCount)}
+        />
+        {data.estimation?.similarEstimateCounts.map((item) => (
+          <Chart key={item.id} value={item.value} max={Math.max(data.estimation.myEstimateCount)} />
         ))}
       </S.ChartWrapper>
       <S.Button onClick={handleShow}>유사 출고 견적 확인하기</S.Button>
--- frontend/src/pages/EstimationPage/HMGArea/index.jsx
+++ frontend/src/pages/EstimationPage/HMGArea/index.jsx
@@ -3,38 +3,10 @@ import SimilarCard from './SimilarCard';
 import * as S from './style';
 
 function HMGArea() {
-  const MOCK_HMGDATA = {
-    trim: '르블랑',
-    price: {
-      min: 43460000,
-      max: 47650000,
-      current: 45800000,
-    },
-    value: 2500,
-    similar: [
-      {
-        id: 1,
-        value: 3422,
-      },
-      {
-        id: 2,
-        value: 3200,
-      },
-      {
-        id: 3,
-        value: 3000,
-      },
-      {
-        id: 4,
-        value: 2800,
-      },
-    ],
-  };
-
   return (
     <S.HMGArea>
       <PriceCard />
-      <SimilarCard data={MOCK_HMGDATA} />
+      <SimilarCard />
     </S.HMGArea>
   );
 }
