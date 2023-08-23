c9d5cb0f9ffa9bf1b31d262be4e69b747784d17a
JeYehyun99 <jo990714@naver.com>
2023-08-23 11:26:39 +0900

    feat: 유사 견적 guide 구현

--- Android/app/src/main/java/com/softeer/cartalog/ui/adapter/ButtonBindingAdapters.kt
+++ Android/app/src/main/java/com/softeer/cartalog/ui/adapter/ButtonBindingAdapters.kt
@@ -1,5 +1,6 @@
 package com.softeer.cartalog.ui.adapter
 
+import android.content.res.ColorStateList
 import android.util.Log
 import android.view.View
 import android.widget.HorizontalScrollView
@@ -9,6 +10,7 @@ import android.widget.RadioButton
 import android.widget.RadioGroup
 import androidx.appcompat.widget.AppCompatButton
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.core.widget.NestedScrollView
 import androidx.databinding.BindingAdapter
 import androidx.fragment.app.FragmentContainerView
@@ -260,4 +262,27 @@ fun setImageCheckBtn(
     button.setOnClickListener {
         nestedScrollView.smoothScrollTo(0, 0)
     }
+}
+
+@BindingAdapter("constraintLayout","imageView")
+fun setInfoBtnClick(
+    infoBtn: ImageView,
+    constraintLayout: ConstraintLayout,
+    guideBgr: ImageView
+){
+    infoBtn.setOnClickListener {
+        if(constraintLayout.visibility == View.GONE){
+            infoBtn.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(infoBtn.context, R.color.white))
+            constraintLayout.visibility = View.VISIBLE
+        } else {
+            infoBtn.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(infoBtn.context, R.color.primary_color_900))
+            constraintLayout.visibility = View.GONE
+        }
+    }
+
+    constraintLayout.setOnClickListener {
+        infoBtn.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(infoBtn.context, R.color.primary_color_900))
+        constraintLayout.visibility = View.GONE
+    }
+    guideBgr.setOnClickListener(null)
 }
